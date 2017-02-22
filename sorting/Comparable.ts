55da0fe89dfb704ebb3ae224a27ef63dee9292b3
Tomasz Dobrowolski <chrumck@gmail.com>
2017-02-21 22:19:59 -0800

    set up mocha with TS

--- sorting/Comparable.ts
+++ sorting/Comparable.ts
@@ -1,17 +1,35 @@
-// TODO: Read how to document TS code and addd the member docs.
 
+/**
+ * Comparable interface.
+ * @template T - The type of the instances which are compared.
+ */
 export interface Comparable<T> {
-   compareTo(other: Comparable<T>) : number;
+  compareTo(other: Comparable<T>): Number;
 }
 
-export class ComparableNumber extends Number{
-   compareTo(other: ComparableNumber) : number {
-      if (this < other) {
-         return -1;
-      }
-      if (this > other) {
-         return 1;
-      }
-      return 0;
-   }
+/**
+ * Extends base Number type with compareTo method.
+ * @extends {Number}
+ */
+export class ComparableNumber {
+  /**
+   * Default constructor.
+   */
+  constructor(public value: number) { }
+
+  /**
+  * Compares two numbers.
+  * @param {ComparableNumber} other - The other number to compare to.
+  * @returns {number} - The result of the comparison: -1, 0, or 1.
+  */
+  compareTo(other: ComparableNumber): Number {
+    if (this.value < other.value) {
+      return -1;
+    }
+    if (this.value > other.value) {
+      return 1;
+    }
+    return 0;
+  }
 }
+
--- test/sorting/test-insertionSort.ts
+++ test/sorting/test-insertionSort.ts
@@ -1,18 +1,42 @@
-import 'mocha';
-import 'chai';
+import "mocha";
+import { expect } from "chai";
 
 import { insertionSort } from "../../sorting/insertionSort";
 import { ComparableNumber } from "../../sorting/Comparable";
 
+
 describe('insertionSort', () => {
 
    it('should not blow up on a single value', () => {
-      
       let sampleData = [new ComparableNumber(123)];
-      insertionSort(sampleData);
-      
-      throw Error("boom");
-
-      // TODO: figure out how to use chai
+      insertionSort<Number>(sampleData);
+      expect(sampleData).to.have.lengthOf(1);
    });
+
+   it('should sort two items', () => {
+      let sampleData = [new ComparableNumber(2), new ComparableNumber(1)];
+      insertionSort(sampleData);
+      expect(sampleData).to.have.lengthOf(2);
+      expect(sampleData[0].value).to.equal(1);
+      expect(sampleData[1].value).to.equal(2);
+   });
+
+   it('should sort three descending items', () => {
+      let sampleData = [new ComparableNumber(3), new ComparableNumber(2), new ComparableNumber(1)];
+      insertionSort(sampleData);
+      expect(sampleData).to.have.lengthOf(3);
+      expect(sampleData[0].value).to.equal(1);
+      expect(sampleData[1].value).to.equal(2);
+      expect(sampleData[2].value).to.equal(3);
+   });
+
+   it('should sort three items out of order', () => {
+      let sampleData = [new ComparableNumber(2), new ComparableNumber(3), new ComparableNumber(1)];
+      insertionSort(sampleData);
+      expect(sampleData).to.have.lengthOf(3);
+      expect(sampleData[0].value).to.equal(1);
+      expect(sampleData[1].value).to.equal(2);
+      expect(sampleData[2].value).to.equal(3);
+   });
+
 });
